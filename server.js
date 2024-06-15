c08f6c5f05cd6ca50636ad95dce5b853630c4ffb
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-06-15 18:25:21 +1000

    added shoes route, supporting query parameters

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const collectibles = [
   { name: "vintage 1970s yogurt SOLD AS-IS", price: 0.99 },
 ];
 
+const shoes = [
+  { name: "Birkenstocks", price: 50, type: "sandal" },
+  { name: "Air Jordans", price: 500, type: "sneaker" },
+  { name: "Air Mahomeses", price: 501, type: "sneaker" },
+  { name: "Utility Boots", price: 20, type: "boot" },
+  { name: "Velcro Sandals", price: 15, type: "sandal" },
+  { name: "Jet Boots", price: 1000, type: "boot" },
+  { name: "Fifty-Inch Heels", price: 175, type: "heel" },
+];
+
 app.get("/greetings/:username", (req, res) => {
   const username = req.params.username;
   res.send(`Hello there, ${username}!`);
@@ -38,6 +48,23 @@ app.get("/collectibles/:number", (req, res) => {
       `So, you want the ${collectible.name}? For ${collectible.price}, it can be yours!`
     );
   }
+});
+
+app.get("/shoes", (req, res) => {
+  const minPrice = req.query["min-price"];
+  const maxPrice = req.query["max-price"];
+  const type = req.query.type;
+  let response = shoes;
+  if (minPrice) {
+    response = response.filter((shoe) => shoe.price > minPrice);
+  }
+  if (maxPrice) {
+    response = response.filter((shoe) => shoe.price < maxPrice);
+  }
+  if (type) {
+    response = response.filter((shoe) => shoe.type === type);
+  }
+  res.send(response);
 });
 
 app.listen(3000);
