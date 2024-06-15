8d18d20e81c1c16adb78f9eceb5e3b5ba01e6e60
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-06-15 18:00:00 +1000

    added the roll number route

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ app.get("/greetings/:username", (req, res) => {
   const username = req.params.username;
   res.send(`Hello there, ${username}!`);
 });
+
+app.get("/roll/:number", (req, res) => {
+  const number = req.params.number;
+
+  if (isNaN(number)) {
+    res.send("You must specify a number.");
+  }
+
+  const randomWholeNumber = Math.floor(Math.random() * number);
+  res.send(`You rolled a ${randomWholeNumber}`);
+});
+
 app.listen(3000);
